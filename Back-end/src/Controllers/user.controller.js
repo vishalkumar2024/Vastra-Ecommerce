dbec88e5a5f9eed9e556c24578c5df02dc55ee09
vishalkumar2024 <vkumarsoni30@gmail.com>
2026-02-27 22:45:38 +0530

    completed registration controller

--- Back-end/src/Controllers/user.controller.js
+++ Back-end/src/Controllers/user.controller.js
@@ -1,5 +1,5 @@
 
-import {UserModel} from '../Models/user.model.js'
+import { UserModel } from '../Models/user.model.js'
 
 
 // Function to login a user
@@ -63,71 +63,53 @@ const login = async (req, res) => {
 const registerUser = async (req, res) => {
 
     // 1. Get user details from frontend ( here from postman).
-    // 2. validation - check if any input field (specially email and userName) is empty or in wrong format.
     // 3. Check if user already exist.
     // // 4. Check if user give his avatar image or not
-    // // 5. If user gave his avatar image then send it to cloudinary ( to store image files)
     // 6. Creating user by userModel.create (CRUD) to store in database.
     // 7. Remove password and refresh token field from response. 
     // 8. Check for user creation- if true then return res.
 
 
-    const { email, fullName, userName, password } = req.body; // 1.
+    const { email, fullName, userName, password } = req.body;
 
     try {
-        if (fullName == "" || email == "" || userName === "" || password == "") { // 2.
-            throw new ApiError(404, "All input fields are not filled")
-        }
-
-        const existedUser = await UserModel.findOne({ // 3.
+        const existedUser = await UserModel.findOne({ 
             $or: [{ email }, { userName }]
         })
         if (existedUser) {
-            throw new ApiError(400, "User already exist with this email or userName ")
+             return res.status(400).json({
+                success: false,
+                message: "User already exist with this email or userName ",
+            })
         }
 
-        // Skipping file upload as of now
-
-        // const avatarlocalPath = req.files?.avatar[0]?.path; // .4
-        // const coverImagelocalPath = req.files?.coverImage[0]?.path;
-
-
-        // if (!avatarlocalPath) {
-        //     throw new ApiError(400, "Avatar is required firstly")
-        // }
-
-        // const avatar = await uploadOnCloudinary(avatarlocalPath)  // 5.
-        // const coverImage = await uploadOnCloudinary(coverImagelocalPath)
-
-        // if (!avatar) {
-        //     throw new ApiError(400, "Avatar is required secondly")
-        // }
-
-
-
-        const user = await UserModel.create({ //.6
+        const user = await UserModel.create({ 
             email,
             password,
             fullName,
-            // avatar: avatar.url,
-            // coverImage: coverImage?.url || "",
             userName: userName.toLowerCase(),
 
         })
 
-        const createdUser = await UserModel.findOne(user._id).select( //.7
+        const createdUser = await UserModel.findOne(user._id).select( 
             "-password -refreshToken"
         )
 
         if (!createdUser) {
-            throw new ApiError(500, "Something went wrong while registering user")
+            return res.status(500).json({
+                success: false,
+                message: "user could not be created",
+            })
         }
 
         return res.status(200).json(createdUser) //.8
     } catch (error) {
-        console.log("The error - ", error)
+        return res.status(500).json({
+            error: error,
+            success: false,
+            message: "Something went wrong while registering user",
+        })
     }
-
 }
 
-export  {login, registerUser}
+export { login, registerUser }
