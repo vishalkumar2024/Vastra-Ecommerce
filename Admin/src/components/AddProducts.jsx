856aea7f7f111a1f3c3587976f3a8e703f7ea115
vishalkumar2024 <vkumarsoni30@gmail.com>
2026-03-01 15:11:46 +0530

    Enhance AddProducts component with improved styling and layout for better user experience

--- Admin/src/components/AddProducts.jsx
+++ Admin/src/components/AddProducts.jsx
@@ -4,42 +4,56 @@ import { IoCloudUploadOutline } from "react-icons/io5";
 
 function AddProducts() {
     return (
-        <div>
-            <div>
-                <div>
-                    <p>Product Title</p>
-                    <input type="text" name="name" placeholder='Type title' />
+        <div className='m-6 py-10 px-10 bg-white w-full'>
+
+            <div className='w-full mb-5 '>
+                <p className='text-[18px] mb-2 font-semibold text-gray-500'>Product Title</p>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder='Type title'
+                    className='border-2 outline-none border-gray-300 w-full p-2 rounded-md'
+                />
+            </div>
+
+            <div className='w-full  flex gap-10 mb-5'>
+                <div className='w-[50%]'>
+                    <p className='text-[18px] mb-2 font-semibold text-gray-500'>Price</p>
+                    <input
+                        type="text"
+                        name="old_price"
+                        placeholder='Type Price'
+                        className='border-2 outline-none border-gray-300 w-full p-2 rounded-md'
+                    />
+                </div>
+                <div className='w-[50%] '>
+                    <p className='text-[18px] mb-2 font-semibold text-gray-500'>Offer Price</p>
+                    <input
+                        type="text"
+                        name="new_price"
+                        placeholder='Type Price'
+                        className='border-2 outline-none border-gray-300 w-full p-2 rounded-md'
+                    />
                 </div>
             </div>
 
-            <div>
-                <div>
-                    <p>Price</p>
-                    <input type="text" name="old_price" placeholder='Type Price' />
-                </div>
-                <div>
-                    <p>Offer Price</p>
-                    <input type="text" name="new_price" placeholder='Type Price' />
-                </div>
-            </div>
-
-            <div>
-                <p>Product category</p>
-                <select name="category" id="">
-                    <option value="women">Women</option>
+            <div className=' w-[20%] mb-5'>
+                <p className='text-[18px] mb-2 font-semibold text-gray-700'>Product category</p>
+                <select className='border-2 border-gray-300 w-[80%] text-gray-500 p-2 rounded-md' name="category" id="">
                     <option value="men">Men</option>
+                    <option value="women">Women</option>
                     <option value="kid">Kid</option>
                 </select>
             </div>
 
-            <div>
+            <div className='py-5 px-6 w-[10%] rounded-sm border-2 border-gray-400 cursor-pointer bg-gray-200 mb-5'>
                 <label htmlFor="file-input">
-                    <IoCloudUploadOutline />
+                    <IoCloudUploadOutline className='size-10 cursor-pointer text-gray-500' />
                 </label>
-                <input type="file" name='image' id='file-input' hidde />
+                <input type="file" name='image' id='file-input' hidden />
             </div>
-            
-            <button>Add</button>
+
+            <button className='text-[17px] uppercase text-white cursor-pointer font-semibold bg-blue-400 rounded-sm  px-10 py-2'>Add</button>
         </div>
 
     )
