63b148de2fa63695b106a69652cbc8446f9c6b55
vishalkumar2024 <vkumarsoni30@gmail.com>
2026-03-02 20:13:06 +0530

    completed logic to get data form addProduct omponent and sen it in backend

--- Admin/src/components/AddProducts.jsx
+++ Admin/src/components/AddProducts.jsx
@@ -1,51 +1,102 @@
 import React, { useState } from 'react'
 import upload from '../components/Assets/upload-image.png'
+import axios from 'axios'
 
 
 function AddProducts() {
 
     const [image, setImage] = useState(false)
-    const handleUpload = (e) =>{
+    const [productDetails, setproductDetails] = useState({
+        name: "",
+        image: "",
+        category: "men",
+        old_price: "",
+        new_price: "",
+    })
+
+    const onChangeHandler = (e) => {
+        setproductDetails({ ...productDetails, [e.target.name]: e.target.value })
+    }
+
+
+    const handleUpload = (e) => {
         setImage(e.target.files[0])
     }
+
+    const handleAddProduct = async () => {
+        try {
+            console.log(productDetails);
+
+            let product = productDetails;
+
+            const formData = new FormData();
+            formData.append("product", image);
+
+            const response = await axios.post(
+                "http://localhost:4000/upload",
+                formData
+            );
+
+            const responseData = response.data;
+
+            if (responseData.success) {
+                product.image = responseData.image_url;
+                console.log(product);
+            }
+
+        } catch (error) {
+            console.error("Upload failed:", error);
+        }
+    };
 
     return (
         <div className='m-6 py-10 px-10 bg-white w-full'>
 
-            <div className='w-full mb-5 '>
-                <p className='text-[18px] mb-2 font-semibold text-gray-500'>Product Title</p>
+            <div className='w-full mb-8 '>
+                <p className='text-[18px] mb-3 font-semibold text-gray-500'>Product Title</p>
                 <input
                     type="text"
                     name="name"
+                    value={productDetails.name}
+                    onChange={onChangeHandler}
                     placeholder='Type title'
                     className='border-2 outline-none border-gray-300 w-full p-2 rounded-md'
                 />
             </div>
 
-            <div className='w-full  flex gap-10 mb-5'>
+            <div className='w-full  flex gap-10 mb-8'>
                 <div className='w-[50%]'>
-                    <p className='text-[18px] mb-2 font-semibold text-gray-500'>Price</p>
+                    <p className='text-[18px] mb-3 font-semibold text-gray-500'>Price</p>
                     <input
                         type="text"
                         name="old_price"
+                        value={productDetails.old_price}
+                        onChange={onChangeHandler}
                         placeholder='Type Price'
                         className='border-2 outline-none border-gray-300 w-full p-2 rounded-md'
                     />
                 </div>
                 <div className='w-[50%] '>
-                    <p className='text-[18px] mb-2 font-semibold text-gray-500'>Offer Price</p>
+                    <p className='text-[18px] mb-3 font-semibold text-gray-500'>Offer Price</p>
                     <input
                         type="text"
                         name="new_price"
+                        value={productDetails.new_price}
+                        onChange={onChangeHandler}
                         placeholder='Type Price'
                         className='border-2 outline-none border-gray-300 w-full p-2 rounded-md'
                     />
                 </div>
             </div>
 
-            <div className=' w-[20%] mb-5'>
+            <div className=' w-[20%] mb-8'>
                 <p className='text-[18px] mb-2 font-semibold text-gray-700'>Product category</p>
-                <select className='border-2 border-gray-300 w-[80%] text-gray-500 p-2 rounded-md' name="category" id="">
+                <select
+                    value={productDetails.category}
+                    onChange={onChangeHandler}
+                    className='border-2 border-gray-300 w-[80%] text-gray-500 p-2 rounded-md'
+                    name="category"
+                    id="">
                     <option value="men">Men</option>
                     <option value="women">Women</option>
                     <option value="kid">Kid</option>
@@ -54,9 +105,9 @@ function AddProducts() {
 
             <div className='w-[10%] flex items-baseline justify-center rounded-sm border-2 border-gray-400 cursor-pointer bg-gray-200 mb-5'>
                 <label htmlFor="file-input">
-                    <img 
-                    src={image?URL.createObjectURL(image):upload}
-                    className='size-20 cursor-pointer text-gray-500' />
+                    <img
+                        src={image ? URL.createObjectURL(image) : upload}
+                        className='size-20 cursor-pointer text-gray-500' />
                 </label>
 
                 <input
@@ -68,7 +119,10 @@ function AddProducts() {
                 />
             </div>
 
-            <button className='text-[17px] uppercase text-white cursor-pointer font-semibold bg-blue-400 rounded-sm  px-10 py-2'>Add</button>
+            <button
+                onClick={handleAddProduct}
+                className='text-[17px] uppercase text-white cursor-pointer font-semibold bg-blue-400 rounded-sm  px-10 py-2'
+            >Add</button>
         </div>
 
     )
