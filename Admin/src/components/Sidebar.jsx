3c889def96a9b67294d93703d075af59719c30fe
vishalkumar2024 <vkumarsoni30@gmail.com>
2026-04-07 12:46:50 +0530

    enhence styling using media query for screen size under 500px

--- Admin/src/components/Sidebar.jsx
+++ Admin/src/components/Sidebar.jsx
@@ -5,9 +5,10 @@ import { SiBookstack } from "react-icons/si";
 
 function Sidebar() {
     return (
-        <div className='flex  flex-col gap-5 pt-5 w-full h-[100vh] max-w-[220px] bg-white max-md:flex-row max-md:w-full max-md:h-full max-md:py-6 max-md:px-0 max-md:max-w-none max-md:justify-center'>
+        <div className='flex  flex-col gap-5 pt-5 w-full h-[100vh] max-w-[220px] bg-red-200 max-md:flex-row max-md:w-full max-md:h-full max-md:py-6 max-md:px-0 max-md:max-w-none max-md:justify-center'>
 
             <Link 
+                id='addProduct'
                 to='/addproduct'
                 className='flex items-center justify-center bg-[#f0ecec] mx-4 gap-4 py-2 px-3 rounded-sm cursor-pointer'
             >
@@ -16,6 +17,7 @@ function Sidebar() {
             </Link>
 
             <Link 
+                id='productList'
                 to='/listproduct'
                 className='flex items-center justify-center mx-4 gap-4 py-2 px-3 rounded-sm bg-[#f0ecec] cursor-pointer'
             >
